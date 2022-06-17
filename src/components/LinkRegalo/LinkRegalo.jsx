0cd93d5637f6595d97d272f74291fd3994c839bb
Lautaro Bustos <uri21199@gmail.com>
2022-06-17 18:52:45 -0300

    actualizacion

--- src/components/LinkRegalo/LinkRegalo.jsx
+++ src/components/LinkRegalo/LinkRegalo.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import {React, useState, useRef} from 'react';
 import './LinkRegalo.css';
 
 const LinkRegalo = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const parentRef = useRef();
+
     return (
-        <div className='regalo'>
-            <h2 className="linkcbu">Link de Regalo</h2>
-        </div>
+            <div className='regalo'>
+                <h2 className="linkcbu">Link de Regalo</h2>
+                <div className='collapsable'>
+                    <button className='toggle' onClick={() => setIsOpen(!isOpen)}>Ver datos bancarios</button>
+                    <div 
+                    className='infobanco-parent' 
+                    ref={parentRef} 
+                    style={
+                        isOpen 
+                        ? {
+                        height: '75px',
+                        textAlign: 'left',
+                    } 
+                    : {
+                        height: '0px',
+                        padding: '0px',
+                        border: '0px',
+                    }
+                }
+                    >
+                        <div className="infobanco"><p>Banco:</p><p>Alias:</p><p>Titular:</p></div>
+                    </div>
+                </div>
+            </div>
     );
 };
 
