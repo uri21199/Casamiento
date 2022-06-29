7567cde47fdde122197592358a32a9f23eb918a1
Lautaro Bustos <uri21199@gmail.com>
2022-06-29 19:46:37 -0300

    actualizacion

--- src/components/Asistencia/Asistencia.jsx
+++ src/components/Asistencia/Asistencia.jsx
@@ -4,7 +4,7 @@ import './Asistencia.css';
 const Asistencia = () => {
     return (
         <div className='formAsistencia'>
-            <a href="#">¡Confirmanos si vas a poder estar haciendo clic acá!</a>
+            <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=kFPQMwsOaUKc7XaKdFi7IF1pxAU8MC9HmNNAMNhphk5UNkU3ODNIU0s3NlNKV1hDTUExSjRDODhIVi4u">¡Confirmanos si vas a poder estar entrando acá!</a>
         </div>
     )
 };
